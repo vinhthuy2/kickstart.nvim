90122408bdf0368bea90797df15d6d69594bb1f7
Vinh Thuy Tran <vinhthuy.tran@zuhlke.com>
2025-07-18 10:30:41 +0700

    modular setup

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,29 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- Optional image support for file preview: See `# Preview Mode` for more information.
+    -- {"3rd/image.nvim", opts = {}},
+    -- OR use snacks.nvim's image module:
+    -- "folke/snacks.nvim",
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  config = function()
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal current left<CR>')
+    require("neo-tree").setup({
+      filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+      hijack_netrw_behavior = "open_current",
+    })
+  end
+}
